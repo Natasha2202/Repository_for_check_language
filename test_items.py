b956383a05cc633269177e1eda96486284fb51c8
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-11-30 13:34:02 +0300

    added test_items.py

--- test_items.py
+++ test_items.py
@@ -6,10 +6,7 @@ def test_get_answer(browser):
     browser.get(link)
     browser.implicitly_wait(5)
     #time.sleep(30) #проверяющему нужно раскомментировать эту строку самостоятельно
-    try:
-        button = browser.find_element_by_css_selector(".btn-add-to-basket")
-        assert button, "the button was found"
-    except:
-        print("the button did not find")
+    element = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert element.is_displayed() == True, "the button did not find"
 
     #пустая строка
